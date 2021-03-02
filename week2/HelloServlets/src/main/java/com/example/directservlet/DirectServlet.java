7a66e75c78a7ba5973cb16ac37725084d35ab242
sophiagavrila <msophiagavrila@gmail.com>
2021-03-02 12:00:13 -0500

    add servlet start

---
+++ week2/HelloServlets/src/main/java/com/example/directservlet/DirectServlet.java
@@ -0,0 +1,61 @@
+package com.example.directservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+public class DirectServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Logger log = Logger.getLogger(DirectServlet.class);
+
+	
+	/*
+	 * 3 Steps to create a Servlet:
+	 * 
+	 * Step 1: create a class that inherits from HttpServlet or GenericServlet
+	 * 
+	 * Step 2: Override the doGet and doPost methods()
+	 * 	- we do NOT override the init() method (abstracted)
+	 * 
+	 * Step 3: Configure the servlet in your web.xml (Deployment Descriptor)
+	 */
+	// This is happeninng in the background 
+//	public void init() throws ServletException {
+//		
+//	}
+	
+	// this method allows a Servlet to handle a GET request
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		
+		// When this servlet recieves a get request, I want it to simply write to the browser, "Hey!"
+		res.setContentType("text/html");
+		
+		// PrintWriter is an object that prints text data to a character stream
+		// we can call it on the response
+		// ctrl + shift + o will import all the things!
+		PrintWriter out = res.getWriter();
+		out.println("<html><body><h1>The Servlet is talking directly to the client!</h1></body></html>");
+		
+		log.info("We're inside the directServlet doGet() method");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
