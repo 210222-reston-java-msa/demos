ec8f82fdd18399133eb5dc0602bf485896ee6c79
sophiagavrila <msophiagavrila@gmail.com>
2021-03-03 16:20:57 -0500

    add sql script

---
+++ week2/EmployeeDBServlets/src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,60 @@
+package com.revature.util;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+public class ConnectionUtil {
+	
+	
+	// this is boilerplate code 
+	private static Logger log = Logger.getLogger(ConnectionUtil.class);
+	
+	public static Connection getConnection() {
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			log.warn("Cannot load the driver");
+			e.printStackTrace();
+		}
+		
+		
+		Properties props = new Properties();
+		// This ClassLoader isn't always necessary, but it's an obj used to search through our entire project
+		// to find our connection.properties file to give us the connection creds
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		Connection conn = null;
+		
+		try {
+			props.load(loader.getResourceAsStream("connection.properties"));
+			// capture the connection URL
+			String url = props.getProperty("url"); 
+			// capture the username
+			String username = props.getProperty("username");
+			// capture the password
+			String password = props.getProperty("password");
+			
+			try {
+				conn = DriverManager.getConnection(url, username, password);
+			} catch (SQLException e) {
+				log.warn("unable to obtain a connection to the database");
+			}
+			
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+
+	
+	
+
+}
---
+++ week2/EmployeeDBServlets/src/main/resources/setup.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS sophiag.employee;
+-- DROP TABLE employee CASCADE; this is possible too
+CREATE TABLE sophiag.employee (
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	username VARCHAR(50),
+	pass_word VARCHAR(50)
+);
+
+INSERT INTO sophiag.employee (first_name, last_name, username, pass_word) 
+	VALUES ('Timmy', 'Maximoff', 'tmax', 'secret');
+
+
