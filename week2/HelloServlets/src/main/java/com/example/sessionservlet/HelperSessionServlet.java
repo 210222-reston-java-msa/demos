d7fb5adbf807763a78002c8629777eb1c65670ed
sophiagavrila <msophiagavrila@gmail.com>
2021-03-03 12:30:25 -0500

    add HelperSessionServlet

--- week2/HelloServlets/src/main/java/com/example/sessionservlet/HelperSessionServlet.java
+++ week2/HelloServlets/src/main/java/com/example/sessionservlet/HelperSessionServlet.java
@@ -1,12 +1,17 @@
 package com.example.sessionservlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.example.model.SuperVillain;
 
+// /helpersesserv
 public class HelperSessionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -33,7 +38,30 @@ public class HelperSessionServlet extends HttpServlet {
 		 * <h1> Villain name </h1> ..... getName()
 		 */
 		
+		HttpSession session = request.getSession();
 		
+		SuperVillain villain = (SuperVillain) session.getAttribute("villain");
+		
+		// We are creating a dynamic html page on the fly
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		
+			if (villain != null) {
+				
+				out.println("We've captured the villain!");
+				
+				out.println("<h1>Villain Name: " + villain.getName() + "</h1><br/>");
+				out.println("<b>Superpower: " + villain.getSuperpower() + "</b><br/>");
+				out.println("<i>Bounty: $" + villain.getBounty() + "</i>");
+				
+			} else {
+				// do something here if no villain was fetched
+				out.println("<i>Can't find any villains...</i>");
+				
+			}
+		
+		out.println("</body></html>");
+
 		
 	}
 
