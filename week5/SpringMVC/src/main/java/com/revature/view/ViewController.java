0104001c66c449b0bd2af8e314043686746a4365
sophiagavrila <msophiagavrila@gmail.com>
2021-03-23 12:53:32 -0400

    view resolver

---
+++ week5/SpringMVC/src/main/java/com/revature/view/ViewController.java
@@ -0,0 +1,7 @@
+package com.revature.view;
+
+public interface ViewController {
+
+	public String index();
+		
+}
---
+++ week5/SpringMVC/src/main/java/com/revature/view/ViewControllerImpl.java
@@ -0,0 +1,24 @@
+package com.revature.view;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("viewController") // The @Controller annotation marks this calss as a web request handler
+public class ViewControllerImpl implements ViewController {
+
+	private static Logger logger = Logger.getLogger(ViewControllerImpl.class);
+	
+	// We are mapping requests to this particular controller (or handler)
+	// This annotation specifies what types of requests this class handles
+	@RequestMapping(value= { "/", "/static/index" }, method=RequestMethod.GET)
+	public String index() {
+		logger.info("Getting index HTML from View Controller");
+		System.out.println("=====================  This is a test  ================================");
+		return null;
+	}
+
+	
+	
+}
