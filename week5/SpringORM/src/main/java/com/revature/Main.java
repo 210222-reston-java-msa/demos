835eac9162952f755fd8e11c1402f41d1356c260
sophiagavrila <msophiagavrila@gmail.com>
2021-03-25 12:03:06 -0400

    add finished ORM demo

--- week5/SpringORM/src/main/java/com/revature/Main.java
+++ week5/SpringORM/src/main/java/com/revature/Main.java
@@ -3,25 +3,28 @@ package com.revature;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.revature.model.Food;
 import com.revature.repository.FoodRepo;
 
 public class Main {
-	
+
 	public static ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-	
-	
-//	public static FoodRepo foodRepo = appContext.getBean("foodRepo", FoodRepo.class);
+
+	public static FoodRepo foodRepo = appContext.getBean("foodRepo", FoodRepo.class);
 
 	public static void main(String[] args) {
-		
-		// We still need to load our Application Context
 
-		
+		insertInitialValues();
+		System.out.println(foodRepo.selectAll());
 
 	}
-	
+
 	public static void insertInitialValues() {
 		// insert food into the db
+
+		Food food1 = new Food("pizza", 200);
+		foodRepo.insert(food1);
+
 	}
 
 }
